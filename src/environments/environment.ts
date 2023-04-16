411180086a0c06fc5dbd079516485f1f780a681d
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-16 14:03:15 -0500

    add firebase env options

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyAMrGnDH34I0li01xpRFc2iW_5ByCNZGfM',
+    authDomain: 'dashboard-f225b.firebaseapp.com',
+    projectId: 'dashboard-f225b',
+    storageBucket: 'dashboard-f225b.appspot.com',
+    messagingSenderId: '28534415832',
+    appId: '1:28534415832:web:6c1d6580f31c14138052ad',
+    measurementId: 'G-BY1QZCKW98',
+  },
+};
