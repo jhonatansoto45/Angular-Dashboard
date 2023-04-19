eb5bc53b9741f961448b417d219e611db297dfeb
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-18 22:14:29 -0500

    move menu to component

---
+++ src/app/shared/index.ts
@@ -0,0 +1,4 @@
+export * from './components/menu/menu.component';
+export * from './components/navbar/navbar.component';
+export * from './services/shared.service';
+export * from './shared.module';
