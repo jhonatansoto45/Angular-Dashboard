6efab0969d18476c091d2708e039c8e17ceaa42a
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-18 22:17:26 -0500

    add conditional rendering depending on path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  currPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        const route = this.router.url.split('/')[1];
+        if (route === 'init') {
+          this.currPage = false;
+        } else {
+          this.currPage = true;
+        }
+      }
+    });
+  }
 }
