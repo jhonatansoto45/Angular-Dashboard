f04c0a5bd98ee4a67a54750f46782999a823ee03
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-04-20 15:19:05 -0500

    Refactor: Card creation in page home

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ScrollItemsComponent } from './components/scroll-items/scroll-items.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
+import { ImagePipe } from './pipe/image.pipe';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { ScrollItemsComponent } from './components/scroll-items/scroll-items.com
     DetailsComponent,
     ProjectsComponent,
     ScrollItemsComponent,
+    CardItemComponent,
+    ImagePipe,
   ],
   imports: [CommonModule, DashboardRoutingModule, DragScrollModule],
 })
