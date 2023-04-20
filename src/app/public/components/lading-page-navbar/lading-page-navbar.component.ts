f154fadfeca475be55a0980f17f2fbb1f9764fe3
david hernandez <davidfelipehernandez1@gmail.com>
2023-04-19 23:19:41 -0500

    add basic landing page nav(developing)

--- src/app/public/components/lading-page-navbar/lading-page-navbar.component.ts
+++ src/app/public/components/lading-page-navbar/lading-page-navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { NgxHideOnScrollModule } from 'ngx-hide-on-scroll';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [AppRoutingModule, NgxHideOnScrollModule],
   selector: 'app-lading-page-navbar',
   templateUrl: './lading-page-navbar.component.html',
   styleUrls: ['./lading-page-navbar.component.scss'],
